d69b4088794251ccc4c956ae2c5c52c59b1f7185
Mahdi Hasas <123617775+MahdiHasan943@users.noreply.github.com>
2023-11-13 08:57:38 +0600

    Update test.js
    added new one

--- test.js
+++ test.js
@@ -307,3 +307,18 @@ const findTotalAges = people.reduce((prev, current) => {
   return prev+current.age
 },0)
 // console.log(findTotalAges);
+
+// program to generate fibonacci series up to n terms
+
+// take input from the user
+const number = parseInt(prompt('Enter the number of terms: '));
+let n1 = 0, n2 = 1, nextTerm;
+
+console.log('Fibonacci Series:');
+
+for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+}
